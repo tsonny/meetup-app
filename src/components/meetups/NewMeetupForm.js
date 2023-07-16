b32b61814650b71c5e87a29af3e850bf2f1ccf8d
Tiffany Sonny <tiffanysonny@gmail.com>
2023-07-15 23:30:51 -0700

    form handling logic

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,32 +1,61 @@
-import Card from '../ui/Card';
-import classes from './NewMeetupForm.module.css';
+import { useRef } from "react";
+import Card from "../ui/Card";
+import classes from "./NewMeetupForm.module.css";
 
-function NewMeetupForm () {
-    return (
-      <Card>
-        <form className={classes.form}>
-          <div className={classes.control}>
-            <labe htmlFor='title'>Meetup Title</labe>
-            <input type='text' required id='title' />
-          </div>
-          <div className={classes.control}>
-            <labe htmlFor='title'>Meetup Image</labe>
-            <input type='url' required id='image' />
-          </div>
-          <div className={classes.control}>
-            <labe htmlFor='address'>Address</labe>
-            <input type='text' required id='address' />
-          </div>
-          <div className={classes.control}>
-            <labe htmlFor='description'>Description</labe>
-            <textarea id='description' required rows='5'></textarea> 
-          </div>
-          <div className={classes.actions}>
-            <button>Add Meetup</button>
-          </div>
-        </form>
-      </Card>
-    );
+function NewMeetupForm() {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+    
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+  };
+
+  console.log(meetupData);
+}
+
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor='title'>Meetup Title</label>
+          <input type='text' required id='title' ref={titleInputRef} />
+        </div>
+        <div className={classes.control}>
+          <labe htmlFor='title'>Meetup Image</labe>
+          <input type='url' required id='image' ref={imageInputRef} />
+        </div>
+        <div className={classes.control}>
+          <labe htmlFor='address'>Address</labe>
+          <input type='text' required id='address' ref={addressInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='description'>Description</label>
+          <textarea
+            id='description'
+            required
+            rows='5'
+            ref={descriptionInputRef}
+          ></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
 }
 
 export default NewMeetupForm;
