3d082d31b63fa5d6d95a30c1932d024a0892e326
Tiffany Sonny <tiffanysonny@gmail.com>
2023-07-15 22:49:58 -0700

    added layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import {Route, Switch} from 'react-router-dom';
-import AllMeetupsPage from './pages/AllMeetups';
-import NewMeetupPage from './pages/NewMeetup';
-import FavoritesPage from './pages/Favorites';
-import MainNavigation from './components/layout/MainNavigation';
-
+import { Route, Switch } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import Layout from "./components/layout/Layout";
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Switch>
-        {" "}
         <Route path='/' exact>
           <AllMeetupsPage />
         </Route>
@@ -20,7 +17,7 @@ function App() {
           <FavoritesPage />
         </Route>
       </Switch>
-    </div>
+    </Layout>
   );
 }
 
--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,19 +1,21 @@
 import classes from "./MeetupItem.module.css";
-
+import Card from "../ui/Card";
 function MeetupItem(props) {
   return (
     <li className={classes.item}>
-      <div>
-        <img className={classes.image} src={props.image} alt={props.title} />
-      </div>
-      <div className={classes.content}>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div className={classes.actions}>
-        <button>To Favorites</button>
-      </div>
+      <Card>
+        <div>
+          <img className={classes.image} src={props.image} alt={props.title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button>To Favorites</button>
+        </div>
+      </Card>
     </li>
   );
 }
